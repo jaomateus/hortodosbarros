b7374d26adc6e58dd80d1480e4c1b3d3671ceb0f
Joao Mateus <jaomateus@gmail.com>
2023-08-11 16:41:44 +0200

    finish error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,11 +1,62 @@
 import React from "react";
+import { Link, useRouteError } from "react-router-dom";
+import { styled } from "styled-components";
+import img from "../assets/not_found.svg";
 
 const Error = () => {
+  const error = useRouteError();
+  console.log(error);
+
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt="not found" />
+          <h3>Oooops...</h3>
+          <p>
+            Looks like the page you're looking for was eaten by one of our
+            catterpillars!
+          </p>
+          <Link to="/">Back Home</Link>
+        </div>
+      </Wrapper>
+    );
+  }
+
   return (
-    <>
-      <h4>Error Page</h4>
-    </>
+    <Wrapper>
+      <div>
+        <h4>Oooooops... Something went wrong!</h4>
+      </div>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  min-height: 100vh;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  img {
+    max-width: 600px;
+    display: block;
+    margin-bottom: 2rem;
+    margin-top: -3rem;
+  }
+  h3 {
+    margin-bottom: 0%.5rem;
+  }
+  p {
+    line-height: 1.5rem;
+    margin-top: 0.5rem;
+    margin-bottom: 1rem;
+    color: var(--clr-grey-6);
+  }
+  a {
+    color: var(--clr-primary-5);
+    text-transform: capitalize;
+  }
+`;
 
 export default Error;
